44bdb41339580f3beae1f3b8a9c5e81e6f4efb5a
PO Boisvert <pob944@hotmail.com>
2025-12-20 17:36:01 -0500

    v3

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from "@/components/hero";
 import { TourCard } from "@/components/tour-card";
 import { WhyChooseUs } from "@/components/why-choose-us";
-
 export default function Home() {
   return (
     <>
